33c66ff5add9682e918cb5703066d2f50ebbe18e
CaioS <caiosaintleo@gmail.com>
2024-01-10 20:35:07 -0300

    caios
    finalizado o crud e icon e cores

---
+++ models/favoritesModel.js
@@ -0,0 +1,36 @@
+const connection = require('../db/connection');
+
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS favoriteSongs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    videoId VARCHAR(255) NOT NULL
+  );
+`;
+
+connection.query(createTableQuery, (err, results) => {
+  if (err) {
+    console.error('Erro ao criar tabela:', err.message);
+  } else {
+    console.log('Tabela criada com sucesso');
+  }
+});
+
+const favoritesModel = {
+  getAllFavorites: (callback) => {
+    connection.query('SELECT * FROM favoriteSongs', callback);
+  },
+
+  addFavorite: (title, videoId, callback) => {
+    const insertQuery = 'INSERT INTO favoriteSongs (title, videoId) VALUES (?, ?)';
+    connection.query(insertQuery, [title, videoId], callback);
+  },
+
+  removeFavorite: (id, callback) => {
+    const deleteQuery = 'DELETE FROM favoriteSongs WHERE id = ?';
+    connection.query(deleteQuery, [id], callback);
+  },
+
+};
+
+module.exports = favoritesModel;
